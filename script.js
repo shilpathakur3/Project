1cdb5716e296e043c11fbae55ded7c1e61121439
[Shilpa Singh] <[singhshipa9920@gmail.com]>
2020-11-29 21:03:14 +0530

    my files

---
+++ script.js
@@ -0,0 +1,15 @@
+const submitButton = document.getElementById('myBtn');
+
+submitButton.addEventListener("click", () => {
+  const email = document.getElementById('emailId').value;
+  const uname = document.getElementById('username').value;
+  if (emailId.length > 0 && name.length > 0) {
+    const object = {
+      name: username,
+      emailId: emailId
+    };
+
+  }
+
+  console.log(emailId, name)
+});
